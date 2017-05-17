f2b47cfa644bd3752ddb7495b2ba7b5e9551e88e
Tony Bowden <tony@tmtm.com>
2017-05-17 09:22:07 +0100

    Also scrape names in Austronesian

--- scraper.rb
+++ scraper.rb
@@ -1,7 +1,8 @@
 require 'wikidata/fetcher'
 
 fr_names = WikiData::Category.new( 'Catégorie:Personnalité politique polynésienne', 'fr').member_titles
+ty_names = WikiData::Category.new( 'Catégorie:Tià māìtihia', 'ty').member_titles
 en_names = WikiData::Category.new( 'Category:Members of the Assembly of French Polynesia', 'en').member_titles
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: en_names, fr: fr_names }, output: false)
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: en_names, fr: fr_names, ty: ty_names })
 
