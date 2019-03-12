1916bb96f9ac8ca6d97a148867a5e5ef0478c834
Tony Bowden <tony@tmtm.com>
2019-03-12 10:37:32 +0000

    Also find via P39s

--- scraper.rb
+++ scraper.rb
@@ -4,5 +4,7 @@ fr_names = WikiData::Category.new( 'Catégorie:Personnalité politique polynési
 ty_names = WikiData::Category.new( 'Catégorie:Tià māìtihia', 'ty').member_titles
 en_names = WikiData::Category.new( 'Category:Members of the Assembly of French Polynesia', 'en').member_titles
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: en_names, fr: fr_names, ty: ty_names })
+query = 'SELECT DISTINCT ?item { ?item p:P39/ps:P39 wd:Q28332780 }'
+ids = EveryPolitician::Wikidata.sparql(query)
 
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { en: en_names, fr: fr_names, ty: ty_names })
